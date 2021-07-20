c9cfa0ea7b3ac63cf5d0401c015b1ef372dc4918
FredericDosSantos <frederic1991_14@hotmail.fr>
2021-07-20 16:14:01 +0200

    Add React router to the homepage

--- planning-webapp/src/components/App/index.js
+++ planning-webapp/src/components/App/index.js
@@ -1,15 +1,16 @@
 // == Import npm
 import React from 'react';
+import { BrowserRouter, Switch , Route } from 'react-router-dom';
 
 
 import './styles.scss';
 
 import Header from '../Header';
-//import LoginForm from '../Login'
+import LoginForm from '../Login'
 import LoginApp from '../Login/loginApp'
-//import Homepage from '../Homepage';
-//import MyComponent from '../Sidebar';
-//import MyCalendar from '../Calendar';
+import Homepage from '../Homepage';
+import MyComponent from '../Sidebar';
+import MyCalendar from '../Calendar';
 import Footer from '../Footer';
 
 
@@ -17,11 +18,22 @@ import Footer from '../Footer';
 
 // == Composant
 const App = () => (
-  <div className="app">
-    <Header />
-    <LoginApp/>
-    <Footer />
-  </div>
+  <BrowserRouter >
+    <div className="app">
+      <Switch>
+        <Route exact path="/"> 
+          <Homepage />
+        </Route>
+
+        <Route path="/login"> 
+          <Header />
+          <LoginApp />
+        </Route>
+      </Switch> 
+
+          <Footer />
+    </div>
+  </BrowserRouter>
 );
 
 // == Export
