8b3cbf32a6baec2d87685504a5e1533a9290b556
Heloise Ouvry <heloiseouvry@gmail.com>
2021-08-02 14:15:15 +0200

    Salary report - Event info

--- planning-webapp/src/components/SalaryReport/index.js
+++ planning-webapp/src/components/SalaryReport/index.js
@@ -15,6 +15,16 @@ const SalaryReport = () => {
   const [endDate, setEndDate] = useState("2021-08-31");
   const [salaryData, setSalaryData] = useState([]);
 
+  const eventColorStyle = (backgroundColor) => {
+    return {
+      display: "inline-block",
+      width: "15px",
+      height: "15px",
+      borderRadius: "15px",
+      backgroundColor,
+    };
+  };
+
   const processData = (data) => {
     console.log("data", data);
     const processedData = data.reduce((acc, val) => {
@@ -30,9 +40,13 @@ const SalaryReport = () => {
       acc[val.id].phases = acc[val.id].phases || [];
       acc[val.id].phases.push({
         title: val.title,
+        event_title: val.event_title,
+        color: val.color,
         type: val.type,
         start_date: val.start_date,
         end_date: val.end_date,
+        duration:
+          (Date.parse(val.end_date) - Date.parse(val.start_date)) / 1000 / 3600,
         salary: val.salary,
       });
       return acc;
@@ -87,43 +101,43 @@ const SalaryReport = () => {
               onChange={(event) => setEndDate(event.target.value)}
             />
           </Form.Field>
+          <Button type="submit" primary>
+            Filtrer
+          </Button>
         </Form.Group>
-        <Button type="submit" primary>
-          Filtrer
-        </Button>
       </Form>
-      <Grid celled="internally">
+
+      <Grid textAlign="center" columns="equal" celled="internally">
         <Grid.Row>
-          <Grid.Column width={3}>Technicien</Grid.Column>
-          <Grid.Column width={10}>Phases</Grid.Column>
-          <Grid.Column width={3}>Total</Grid.Column>
+          <Grid.Column width={2}>Technicien</Grid.Column>
+          <Grid.Column width={10}>Date</Grid.Column>
+          <Grid.Column width={2}>Total</Grid.Column>
         </Grid.Row>
 
         {salaryData.map((tech) => (
           <Grid.Row key={tech.id}>
-            <Grid.Column width={3}>
+            <Grid.Column width={2}>
               {tech.firstname} {tech.lastname}
             </Grid.Column>
-            <Grid.Column width={10}>
-              <ul>
-                {tech.phases.map((phase) => (
-                  <li key={phase.title}>
-                    {phase.title} - {phase.salary}€
-                  </li>
-                ))}
-              </ul>
-            </Grid.Column>
-            <Grid.Column width={3}>
+
+            {tech.phases.map((phase) => (
+              <Grid.Column key={phase.title}>
+                <div>
+                  <div style={eventColorStyle(phase.color)}>&nbsp;</div>{" "}
+                  {phase.event_title}
+                </div>
+                <div>{phase.title}</div>
+                <div>
+                  {phase.duration}h - {phase.salary}€
+                </div>
+              </Grid.Column>
+            ))}
+            <Grid.Column width={2}>
               <p>{tech.phases.length} jours travaillés</p>
               <p>{tech.total} €</p>
             </Grid.Column>
           </Grid.Row>
         ))}
-        <Grid.Row>
-          <Grid.Column width={3}></Grid.Column>
-          <Grid.Column width={10}></Grid.Column>
-          <Grid.Column width={3}></Grid.Column>
-        </Grid.Row>
       </Grid>
     </div>
   );
