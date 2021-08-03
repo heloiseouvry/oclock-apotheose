759405c53c0653c45d9f6f2988a4b473ae6d9f72
Heloise Ouvry <heloiseouvry@gmail.com>
2021-08-03 16:51:35 +0200

    Clean code Calendar and TechCalendar

--- planning-webapp/src/components/TechCalendar/index.js
+++ planning-webapp/src/components/TechCalendar/index.js
@@ -1,17 +1,11 @@
 // import React from 'react';
 import React, { useCallback, useRef, useState, useEffect } from "react";
 import TUICalendar from "@toast-ui/react-calendar";
-import { Button, Modal, Header, Image, Icon } from "semantic-ui-react";
+import { Button } from "semantic-ui-react";
 import axios from "axios";
-
-import data from "../../data/data.js";
 
 // import Calendar from '@toast-ui/react-calendar';
 import "tui-calendar/dist/tui-calendar.css";
-
-// If you use the default popups, use this.
-import "tui-date-picker/dist/tui-date-picker.css";
-import "tui-time-picker/dist/tui-time-picker.css";
 
 import "./styles.scss";
 
@@ -23,13 +17,6 @@ const base_url = `http://${host}:${port}/${router}`;
 const TechCalendar = () => {
   const [events, setEvents] = useState([]);
   const [phases, setPhases] = useState([]);
-
-  const onBeforeCreateSchedule = (e) => {
-    //console.log("onBeforeCreateSchedule e", e);
-    // e.guide.clearGuideElement(); il like a preventDefault
-    e.guide.clearGuideElement();
-    return;
-  };
 
   useEffect(() => {
     const getAllEvents = async () => {
@@ -66,10 +53,27 @@ const TechCalendar = () => {
         });
         let phasesToAdd = [];
         for (const phaseBack of response.data) {
-          // console.log("phaseBack", phaseBack);
+          const techInfoResponse = await axios.get(
+            `${base_url}/phases/${phaseBack.id}/techsinfo`,
+            {
+              headers: {
+                Authorization: `bearer ${localStorage.getItem("token")}`,
+              },
+            }
+          );
+          let attendees = [];
+          if (techInfoResponse.data) {
+            for (const tech of techInfoResponse.data) {
+              attendees.push(
+                `${tech.firstname} ${tech.lastname[0]}. (${tech.phone_number})`
+              );
+            }
+          }
+
           const start_date = new Date(phaseBack.start_date);
           const end_date = new Date(phaseBack.end_date);
           let category,
+            location,
             isAllDay = null;
           if (phaseBack.type === "event") {
             category = "allday";
@@ -77,13 +81,16 @@ const TechCalendar = () => {
           } else {
             category = "time";
             isAllDay = false;
+            location = phaseBack.internal_location;
           }
           let phaseFront = {
             id: phaseBack.id.toString(),
             calendarId: phaseBack.event_id.toString(),
+            attendees,
             category: category,
             isVisible: true,
             isAllDay,
+            location,
             title: phaseBack.title,
             body: phaseBack.comments,
             start: start_date,
@@ -96,12 +103,13 @@ const TechCalendar = () => {
                 zip_code: phaseBack.zip_code,
                 city: phaseBack.city,
               },
+              tech_manager_contact: phaseBack.tech_manager_contact,
+              provider_contact: phaseBack.provider_contact,
+              techInfo: techInfoResponse.data,
             },
             color: "#ffffff",
-            bgColor: phaseBack.color
+            bgColor: phaseBack.color,
           };
-          // console.log("phaseFront", phaseFront);
-          // console.log("phaseFront address", phaseFront["raw"]["address"]);
 
           phasesToAdd.push(phaseFront);
         }
@@ -115,8 +123,6 @@ const TechCalendar = () => {
   }, []);
 
   const cal = useRef(null);
-
-  // passer setIsOpen(true) passe ma variable modalIsOpen à true, je peux donc la modifier, mais je ne peux pas utliser ma variable ici, elle est passé ne props à mon composant
 
   function dayView() {
     cal.current.calendarInst.changeView("day", true);
@@ -145,8 +151,6 @@ const TechCalendar = () => {
   const onClickSchedule = useCallback((e) => {
     const { calendarId, id } = e.schedule;
     const el = cal.current.calendarInst.getElement(id, calendarId);
-
-    console.log("onClickSchedule", e, el.getBoundingClientRect());
   }, []);
 
   function getFormattedTime(time) {
@@ -182,115 +186,16 @@ const TechCalendar = () => {
     return html.join("");
   }
 
-  // const onSubmitCreate = (eventFromForm) => {
-  //   var schedule = {
-  //     id: String(Math.random()),
-  //     title: eventFromForm.target.name.value,
-  //     isAllDay: false,
-  //     // les 2 lignes d'après sont attribuées dans onCreateSchedule
-  //     //TODO à récupérer depuis le event.target
-  //     start: modalStartDate,
-  //     end: modalEndDate,
-  //     category: "time",
-  //     // Dans l'objet raw je place les données qui ne sont pas prévues par la librairie et que je vais rendre via le template
-  //     // Exemple : salaire, véhicule
-  //     raw: {
-  //       techID: parseInt(eventFromForm.target.techName.value, 10),
-  //     },
-  //     calendarId: "1",
-  //     // Schedule.body is basic text, not possible to put anything but string
-  //     body: "test",
-  //   };
-  //   /* step2. save schedule */
-  //   // @ts-ignore: Object is possibly 'null'.
-
-  //   // TODO ici je dois prévoir d'envoyer le schedule à sauvegarder en BDD
-  //   //potientiellement ajax.post
-  //   //avec une fausse ID
-  //   //auquel je passe une callback avec ce qu'il y a à executer en cas de retour valide j'execute le createschedule (ligne juste en dessous) qui comportera tous l'objet schedule definitif avec l'ID généré par le back
-  //   cal.current.calendarInst.createSchedules([schedule]);
-  // };
-
-  // const onSubmitUpdate = (eventFromForm) => {
-  //   console.log(
-  //     "eventFromForm.target.IDHidden.value",
-  //     parseFloat(eventFromForm.target.IDHidden.value)
-  //   );
-  //   console.log(
-  //     "eventFromForm.target.calendarIdHidden.value",
-  //     eventFromForm.target.calendarIdHidden.value
-  //   );
-  //   console.log(
-  //     "eventFromForm.target.name.value",
-  //     eventFromForm.target.name.value
-  //   );
-  //   console.log(
-  //     "eventFromForm.target.techName.value",
-  //     eventFromForm.target.techName.value
-  //   );
-
-  //   // TODO check informations I send
-  //   cal.current.calendarInst.updateSchedule(
-  //     parseFloat(eventFromForm.target.IDHidden.value),
-  //     parseInt(eventFromForm.target.calendarIdHidden.value, 10),
-  //     {
-  //       title: eventFromForm.target.name.value,
-  //       raw: {
-  //         techID: parseInt(eventFromForm.target.techName.value, 10),
-  //       },
-  //     }
-  //   );
-  // };
-
-  // const onSubmitEvent = useCallback((event) => {
-  //   event.preventDefault(event);
-  //   // ici avec event.target.techName.value je récupère l'ID de mon tech depuis le select du Form.js
-  //   console.log(
-  //     "je suis event.target.techName.value",
-  //     event.target.techName.value
-  //   );
-  //   console.log("je suis modalStartDate", modalStartDate);
-  //   console.log("je suis modalEndDate", modalEndDate);
-
-  //   //TODO faure le test pour appeler le bon onSubmit
-
-  //   console.log(
-  //     "event.target.createHidden.value",
-  //     event.target.createHidden.value
-  //   );
-  //   if (event.target.createHidden.value == "false") {
-  //     console.log("uptdate");
-  //     onSubmitUpdate(event);
-  //   } else {
-  //     console.log("create");
-  //     onSubmitCreate(event);
-  //   }
-  //   setEditingPhase(null);
-  //   closeChoiceModal();
-  // }, []);
-
   // Le template sert à rendre la vue de la phase, j'y place toutes les infos que je reçois du form via la fonction popupDetailBody(phaseDetails)
   const templates = {
     time: function (schedule) {
-      console.log("time", schedule);
       return getTimeTemplate(schedule, false);
     },
     popupDetailBody: (phaseDetails) => {
       console.log(`popupDetailBody`, phaseDetails);
       var ret = "<div>" + phaseDetails.body;
+      ret += "<p><strong>" + phaseDetails.raw?.type + "</strong></p>";
       ret += "<ul>";
-
-      //Sert à afficher les prénom et le nom du technicien sélectionné dans le form de la modal
-      var techFound = data.find((elementTech) => {
-        console.log("tech Find=", elementTech);
-        console.log("elementTech.id=", elementTech.id);
-        console.log("phaseDetails.raw.techID=", phaseDetails.raw?.techID);
-        return elementTech.id === phaseDetails.raw?.techID;
-      });
-      console.log("techFound=", techFound);
-
-      ret += "<li>" + techFound?.prenom + " " + techFound?.nom + "</li>";
-
       ret += "</ul>";
       ret += "</div>";
       return ret;
@@ -299,12 +204,12 @@ const TechCalendar = () => {
 
   return (
     <div className="App TechCalendar">
-      <Button size='mini' content="<" secondary onClick={prevView} />
-      <Button size='mini' content="Jour" secondary onClick={dayView} />
-      <Button size='mini' content="Semaine" secondary onClick={weekView} />
-      <Button size='mini' content="Mois" secondary onClick={monthView} />
-      <Button size='mini' content=">" secondary onClick={nextView} />
-      <Button size='mini' content="Aujourd'hui" secondary onClick={todayView} />
+      <Button size="mini" content="<" secondary onClick={prevView} />
+      <Button size="mini" content="Jour" secondary onClick={dayView} />
+      <Button size="mini" content="Semaine" secondary onClick={weekView} />
+      <Button size="mini" content="Mois" secondary onClick={monthView} />
+      <Button size="mini" content=">" secondary onClick={nextView} />
+      <Button size="mini" content="Aujourd'hui" secondary onClick={todayView} />
 
       <TUICalendar
         ref={cal}
@@ -314,6 +219,7 @@ const TechCalendar = () => {
           startDayOfWeek: 1,
           daynames: ["Dim.", "Lun.", "Mar.", "Mer.", "Jeu.", "Ven.", "Sam."],
         }}
+        isReadOnly={true}
         taskView={false}
         useCreationPopup={false} // "false" to use our form instead of app Popup
         useDetailPopup={true}
@@ -321,11 +227,9 @@ const TechCalendar = () => {
         calendars={events}
         schedules={phases}
         onClickSchedule={onClickSchedule}
-        onBeforeCreateSchedule={onBeforeCreateSchedule}
       />
     </div>
   );
 };
 
-// export default Calendar;
 export default TechCalendar;
