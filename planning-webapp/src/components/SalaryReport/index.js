e5d8765923bf7dd155d434ea5f926a1dba44ae3f
Heloise Ouvry <heloiseouvry@gmail.com>
2021-08-05 21:34:39 +0200

    Fix current month on salary report

--- planning-webapp/src/components/SalaryReport/index.js
+++ planning-webapp/src/components/SalaryReport/index.js
@@ -5,13 +5,17 @@ import axios from "axios";
 
 import "./styles.scss";
 
-import {admin_url} from "../../../config/dbConf";
+import { admin_url } from "../../../config/dbConf";
 
 const SalaryReport = () => {
-  const currentMonth = new Date().getMonth();
-  const nextMonth = new Date().getMonth() + 1;
-  const [startDate, setStartDate] = useState(`${new Date().getFullYear()}-${("0" + currentMonth).slice(-2)}-01`);
-  const [endDate, setEndDate] = useState(`${new Date().getFullYear()}-${("0" + nextMonth).slice(-2)}-01`);
+  const currentMonth = new Date().getMonth() + 1;
+  const nextMonth = new Date().getMonth() + 2;
+  const [startDate, setStartDate] = useState(
+    `${new Date().getFullYear()}-${("0" + currentMonth).slice(-2)}-01`
+  );
+  const [endDate, setEndDate] = useState(
+    `${new Date().getFullYear()}-${("0" + nextMonth).slice(-2)}-01`
+  );
   const [salaryData, setSalaryData] = useState([]);
   const [selectedDates, setSelectedDates] = useState([]);
 
@@ -52,8 +56,9 @@ const SalaryReport = () => {
         type: val.type,
         start_date: val.start_date,
         end_date: val.end_date,
-        duration:
-          Math.floor((Date.parse(val.end_date) - Date.parse(val.start_date)) / 1000 / 3600),
+        duration: Math.floor(
+          (Date.parse(val.end_date) - Date.parse(val.start_date)) / 1000 / 3600
+        ),
         salary: val.salary,
       });
       return acc;
@@ -90,7 +95,7 @@ const SalaryReport = () => {
 
   return (
     <div>
-      <h1>Salary Report</h1>
+      <h1>Synthèse des salaires</h1>
       <Form onSubmit={handleSubmit}>
         <Form.Group>
           <Form.Field inline required>
@@ -123,55 +128,60 @@ const SalaryReport = () => {
         </Form.Group>
       </Form>
 
-      {selectedDates.length ?
-      <Grid textAlign="center" columns="equal" celled="internally">
-        <Grid.Row>
-          <Grid.Column width={1}>Technicien</Grid.Column>
-          {selectedDates.map((date) => (
-            <Grid.Column key={date}>
-              <p>{`${new Date(date).toLocaleDateString()}`}</p>
-              <p>
-                {`${("0" + new Date(date).getHours()).slice(-2)}h${(
-                  "0" + new Date(date).getMinutes()
-                ).slice(-2)}`}
-              </p>
-            </Grid.Column>
-          ))}
-          {/* const date = (new Date(val.start_date)).toLocaleDateString(); */}
-          <Grid.Column width={1}>Total</Grid.Column>
-        </Grid.Row>
-
-        {salaryData.map((tech) => (
-          <Grid.Row key={tech.id}>
-            <Grid.Column width={1}>
-              {tech.firstname} {tech.lastname}
-            </Grid.Column>
-
+      {selectedDates.length ? (
+        <Grid textAlign="center" columns="equal" celled="internally">
+          <Grid.Row>
+            <Grid.Column width={1}>Technicien</Grid.Column>
             {selectedDates.map((date) => (
               <Grid.Column key={date}>
-                {tech.phases.find((elem) => {if(elem.start_date === date) {phase=elem; return elem;}}) ? (
-                  <React.Fragment>
-                    <div>
-                      <div style={eventColorStyle(phase.color)}>&nbsp;</div>{" "}
-                      {phase.event_title}
-                    </div>
-                    <div>{phase.title}</div>
-                    <div>
-                      {phase.duration}h - {phase.salary}€
-                    </div>
-                  </React.Fragment>
-                ) : null}
+                <p>{`${new Date(date).toLocaleDateString()}`}</p>
+                <p>
+                  {`${("0" + new Date(date).getHours()).slice(-2)}h${(
+                    "0" + new Date(date).getMinutes()
+                  ).slice(-2)}`}
+                </p>
               </Grid.Column>
             ))}
-
-            <Grid.Column width={1}>
-              <p>{tech.phases.length} jours travaillés</p>
-              <p>{tech.total} €</p>
-            </Grid.Column>
+            {/* const date = (new Date(val.start_date)).toLocaleDateString(); */}
+            <Grid.Column width={1}>Total</Grid.Column>
           </Grid.Row>
-        ))}
-      </Grid>
-      : null }
+
+          {salaryData.map((tech) => (
+            <Grid.Row key={tech.id}>
+              <Grid.Column width={1}>
+                {tech.firstname} {tech.lastname}
+              </Grid.Column>
+
+              {selectedDates.map((date) => (
+                <Grid.Column key={date}>
+                  {tech.phases.find((elem) => {
+                    if (elem.start_date === date) {
+                      phase = elem;
+                      return elem;
+                    }
+                  }) ? (
+                    <React.Fragment>
+                      <div>
+                        <div style={eventColorStyle(phase.color)}>&nbsp;</div>{" "}
+                        {phase.event_title}
+                      </div>
+                      <div>{phase.title}</div>
+                      <div>
+                        {phase.duration}h - {phase.salary}€
+                      </div>
+                    </React.Fragment>
+                  ) : null}
+                </Grid.Column>
+              ))}
+
+              <Grid.Column width={1}>
+                <p>{tech.phases.length} jours travaillés</p>
+                <p>{tech.total} €</p>
+              </Grid.Column>
+            </Grid.Row>
+          ))}
+        </Grid>
+      ) : null}
     </div>
   );
 };
